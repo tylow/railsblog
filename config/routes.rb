09cf15215fc367d9c6663225676bf1ecb687e41b
Thitacitta Low <tylow@berkeley.edu>
2016-08-13 16:46:09 -0700

    Generated Posts controller. Working on creating new posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "/pages/about" => "pages#about", as: :about
   get "/pages/contact" => "pages#contact", as: :contact
 
+  get "/posts" => "posts#index"
+  get "/posts/new" => "posts#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
